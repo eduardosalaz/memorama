412470c494e4860e0ba48f1a5d8b5e1615ea1a82
erick-romero <erick.romeroar@gmail.com>
2021-05-13 15:41:58 -0500

    added back button in scrum and pmi

--- src/views/Scrum.vue
+++ src/views/Scrum.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="container.fluid text-center" style="height:100px">
+        <div class="row">
+            <div class="col-sm-1">
+                <div class="container" style="margin-left: 30px">
+                    <router-link to = "/aprende"> <a href=""><img src="..\assets\flecha.png" alt=""></a>  </router-link>
+                </div>
+            </div>
+        </div>
+</div>
   <concepto/>
 </template>
 
